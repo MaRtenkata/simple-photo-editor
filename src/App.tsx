683897909508f9b108dc5c1cb3c35e2f9cb58b03
Martin Shindarski <martin.shindarski@outlook.com>
2024-04-09 12:46:53 +0300

    grid-leyout setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
 import './App.css';
 
-function App() {}
+function App() {
+  return (
+    <div className='container'>
+      <div className='image-box'>Image</div>
+      <div className='sidebar'>Sidebar</div>
+      <div className='slider'>Slider</div>
+    </div>
+  );
+}
 
 export default App;
