e1ec323dad6f9f5d16f586721cecf122adb9d918
Martin Shindarski <martin.shindarski@outlook.com>
2024-04-11 11:41:05 +0300

    added the filter functionality

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,100 @@ import Slider from './component/Slider';
 import UploadInput from './component/UploadInput';
 import { RootState } from './store/store';
 import { clearFileMetadata } from './features/fileSlice';
+import React, { useState } from 'react';
+
+const DEFAULT_OPTIONS = [
+  {
+    name: 'blur',
+    value: 0,
+    min: 0,
+    max: 5,
+    unit: 'px',
+  },
+  {
+    name: 'brightness',
+    value: 100,
+    min: 0,
+    max: 100,
+    unit: '%',
+  },
+  {
+    name: 'contrast',
+    value: 100,
+    min: 0,
+    max: 200,
+    unit: '%',
+  },
+  {
+    name: 'grayscale',
+    value: 0,
+    min: 0,
+    max: 100,
+    unit: '%',
+  },
+  {
+    name: 'hue-rotate',
+    value: 0,
+    min: 0,
+    max: 360,
+    unit: 'deg',
+  },
+  {
+    name: 'invert',
+    value: 0,
+    min: 0,
+    max: 100,
+    unit: '%',
+  },
+  {
+    name: 'opacity',
+    value: 100,
+    min: 0,
+    max: 100,
+    unit: '%',
+  },
+  {
+    name: 'saturate',
+    value: 100,
+    min: 0,
+    max: 200,
+    unit: '%',
+  },
+  {
+    name: 'sepia',
+    value: 0,
+    min: 0,
+    max: 100,
+    unit: '%',
+  },
+];
 
 function App() {
+  const [selectedOptionIndex, setSelectedOptionIndex] = useState(0);
+
   const { file } = useSelector((state: RootState) => state.file);
 
+  const [filterOptions, setFilterOptions] = useState(DEFAULT_OPTIONS);
+
   const dispatch = useDispatch();
+
+  function getFilters() {
+    const filterFunctions = filterOptions.map((option) => {
+      return `${option.name}(${option.value}${option.unit})`;
+    });
+
+    return filterFunctions.join(' ');
+  }
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setFilterOptions((prevOption) => {
+      return prevOption.map((option, index) => {
+        if (selectedOptionIndex !== index) return option;
+
+        return { ...option, value: Number(e.target.value) };
+      });
+    });
+  }
 
   return (
     <div className='container'>
@@ -16,9 +105,10 @@ function App() {
         {file !== null ? (
           <div>
             <img
-              className='image'
-              src={URL.createObjectURL(file)}
-              alt={file.name}
+              className={`image ${file && 'image-filtered'}`}
+              src={file && URL.createObjectURL(file)}
+              alt={file?.name || 'Uploaded Image'}
+              style={{ '--filters': getFilters() } as React.CSSProperties}
             />
             <button onClick={() => dispatch(clearFileMetadata())}>
               Remove Image
@@ -28,9 +118,27 @@ function App() {
           <UploadInput />
         )}
       </div>
-      <div className='sidebar'>Sidebar</div>
+      <div className='sidebar'>
+        {filterOptions.map((option, index) => {
+          return (
+            <button
+              onClick={() => setSelectedOptionIndex(index)}
+              className={`sidebar-btn ${
+                selectedOptionIndex === index ? 'active' : ' '
+              }`}
+            >
+              {option.name.toUpperCase()}
+            </button>
+          );
+        })}
+      </div>
 
-      <Slider />
+      <Slider
+        min={filterOptions[selectedOptionIndex].min}
+        max={filterOptions[selectedOptionIndex].max}
+        value={filterOptions[selectedOptionIndex].value}
+        handleChange={handleChange}
+      />
     </div>
   );
 }
--- src/component/Slider.tsx
+++ src/component/Slider.tsx
@@ -1,7 +1,23 @@
-export default function Slider() {
+interface SliderProps {
+  min: number;
+  max: number;
+  value: number;
+  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export default function Slider(props: SliderProps) {
+  const { min, max, value, handleChange } = props;
+
   return (
     <div className='slider-container'>
-      <input className='slider' type='range' min='0' max='100' />
+      <input
+        onChange={handleChange}
+        className='slider'
+        value={value}
+        type='range'
+        min={min}
+        max={max}
+      />
     </div>
   );
 }
